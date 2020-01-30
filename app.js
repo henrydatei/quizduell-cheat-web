8cc47395a628f38488bbcd9adc54cd9d193a7163
henrydatei <henrydatei@web.de>
2020-01-30 13:04:25 +0100

    post daten werden jetzt von /test nach /games übertragen

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var testRouter = require('./routes/test');
+//var gamesRouter = require('./routes/games');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,9 +22,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/test', testRouter);
+//app.use('/games', gamesRouter);
+
+app.post('/games', function(req, res, next) {
+  var username=req.body.username;
+  var password=req.body.password;
+  res.render('games', { username: username, password: password });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
