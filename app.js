bd77a4284663a45ea6c951a16c308cd2e804c937
Henry Haustein <henrydatei@web.de>
2020-01-31 16:45:37 +0100

    Unterscheidung yourturn/notyourturn

--- app.js
+++ app.js
@@ -39,9 +39,10 @@ app.post('/games', function(req, res, next) {
   var password=req.body.password;
   var gameIDs = ["123","456","789","101112","131415"];
   var opponents = ["Clemens1","Clemens2","Clemens3","Clemens4","Clemens5"];
+  var yourturns = [true, true, false, true, false, false];
   var points = ["1:1","2:2","3:3","4:4","5:5"];
   var rounds = ["1","2","3","4","5"];
-  res.render('games', { username: username, password: password, numberOfGames: 5, gameIDs: gameIDs, opponents: opponents, points: points, rounds: rounds });
+  res.render('games', { username: username, password: password, numberOfGames: 5, gameIDs: gameIDs, opponents: opponents, points: points, rounds: rounds, yourturns: yourturns });
 });
 
 // catch 404 and forward to error handler
